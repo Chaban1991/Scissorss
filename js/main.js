36c7e095f70a6a628bb5a9d4707dcf1e606469f6
nikler <niklerweb@gmail.com>
2015-10-16 19:19:19 +0300

    prototype slider 1

--- js/main.js
+++ js/main.js
@@ -11,4 +11,74 @@ $(document).ready(function(){
 	 return false;
 	});
 
+	
+	$('.pag__item').on('click', function(e) {
+	 	e.preventDefault();
+
+	 	var
+	 		$this = $(this),
+	 		container = $this.closest('.services__content'),
+	 		list = container.find('.info__wrap'),
+	 		item = container.find('.info__text'),
+	 		activeSlide = item.filter('.info-active'),
+	 		nextSlide = activeSlide.next(),
+	 		prevSlide = activeSlide.prev(),
+	 		firstSlide = item.first(),
+	 		lastSlide = item.last(),
+	 		sliderOffset = list.offset().left,
+	 		icon = container.find('.services__icon'),
+	 		activeIcon = icon.filter('.services-active'),
+	 		nextActive = activeIcon.next(),
+	 		prevActive = activeIcon.prev(),
+	 		reqPos = 0;
+
+		if ($(this).hasClass('pag__right')) {
+			if (nextSlide.length) {
+
+				reqPos = nextSlide.offset().left - sliderOffset;
+
+				nextSlide.addClass('info-active').siblings().removeClass('info-active');
+
+				nextActive.addClass('services-active').siblings().removeClass('services-active');
+			} else {
+				return false;
+			}
+
+		} else {
+			if (prevSlide.length) {
+
+				reqPos = prevSlide.offset().left - sliderOffset;
+
+				prevSlide.addClass('info-active').siblings().removeClass('info-active');
+
+				prevActive.addClass('services-active').siblings().removeClass('services-active');
+			} else {
+				return false;
+			}
+
+		}
+	 	
+	 	list.css('left','-' + reqPos +'px');
+
+	 });
+	
+	var size = 370,
+    newsContent = $('.info__text'),
+    i = 0;
+    
+
+    for (i = 0; i < newsContent.length; i++) {
+    	newsText = newsContent.eq(i).text();
+
+		if(newsText.length > size){
+			newsContent.eq(i).text(newsText.slice(0, size) + ' ...');
+		}
+
+			console.log(newsContent.eq(i).text().length);
+	}	
+
+
+
 });
+
+
